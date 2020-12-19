f911457caf870907165038a56327651f22d54a6f
hugo BOGRAND <h.bogrand@gmail.com>
2020-12-19 15:12:17 +0100

    avion 1.2(hugo)

--- cdaProjetAvion/src/cda/poo/objects/Avion.java
+++ cdaProjetAvion/src/cda/poo/objects/Avion.java
@@ -2,26 +2,28 @@ package cda.poo.objects;
 
 public class Avion {
 
-	private final String NAME = "PEGASUS";
+	private final String nom = "PEGASUS";
 	private final int nombreVieInit = 5;
+
 	private int nombreVie = nombreVieInit;
-	private int x, y;
+	private int posX; // seul la position lateral nous interesse
+
 	private boolean isAlive = true;
+	private boolean isTouch = false;
 
 	public Avion() {
 	}
-	
+
 	public int getPositionX() {
-		return this.x;
+		return this.posX;
 	}
 
-	public int getPositionY() {
-		return this.y;
+	public void turnRight() {
+		this.posX += 20;
 	}
 
-	public void setPosition(int pX, int pY) {
-		this.x += pX;
-		this.y += pY;
+	public void turnLeft() {
+		this.posX -= 20;
 	}
 
 	public int getNombreVie() {
@@ -30,9 +32,15 @@ public class Avion {
 
 	public void setNombreVie(int degat) {
 		this.nombreVie -= degat;
-		if (nombreVie == 0) {
-			setAlive(false);
+		if (nombreVie < 1) {
+			setAlive(false); //partie terminée
 		}
+	}
+
+	public void activationBouclier() throws InterruptedException {
+		if (this.isTouch = true)
+			setNombreVie(0);
+		Thread.sleep(10000);
 	}
 
 	public boolean isAlive() {
