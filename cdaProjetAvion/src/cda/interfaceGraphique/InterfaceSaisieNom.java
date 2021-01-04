77561e3602ba49f283bd3d42779c5c44d3fe2ab9
hugo BOGRAND <h.bogrand@gmail.com>
2021-01-04 18:06:52 +0100

    deplacement new joueur,getscore et getjoueur (interface jeu to interface avant jeu)

--- cdaProjetAvion/src/cda/interfaceGraphique/InterfaceSaisieNom.java
+++ cdaProjetAvion/src/cda/interfaceGraphique/InterfaceSaisieNom.java
@@ -20,9 +20,6 @@ public class InterfaceSaisieNom {
 	private JFrame frame;
 	private static Joueur joueurActuel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 
 		EventQueue.invokeLater(new Runnable() {
@@ -39,20 +36,13 @@ public class InterfaceSaisieNom {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public InterfaceSaisieNom() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(600, 350, 650, 300);
-		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		// début fieldNomJoueur ***********************************************
@@ -63,13 +53,7 @@ public class InterfaceSaisieNom {
 		fieldNomJoueur.setFont(new Font("Times New Roman", Font.BOLD, 17));
 		fieldNomJoueur.setHorizontalAlignment(SwingConstants.CENTER);
 		fieldNomJoueur.setBounds(69, 89, 486, 48);
-
-		// on ajoute le label a la JFrame
 		frame.getContentPane().add(fieldNomJoueur);
-
-		// fin fieldNomJoueur ***********************************************
-
-		// début bouttonOk ***********************************************
 
 		JButton bouttonOk = new JButton("OK");
 		bouttonOk.addMouseListener(new MouseAdapter() {
@@ -81,7 +65,6 @@ public class InterfaceSaisieNom {
 
 				if (nomSaisi.length() < 3 || nomSaisi.length() > 6) {
 					fieldNomJoueur.setText("");
-
 				}
 				if (nomSaisi.length() >= 3 && nomSaisi.length() <= 6) {
 
@@ -95,44 +78,27 @@ public class InterfaceSaisieNom {
 					InterfaceAvantJeu.closeFrame();
 					frame.setVisible(false);
 				}
-				// fin test nomSaisi ***********************************************
 			}
 		});
 		bouttonOk.setBackground(Color.LIGHT_GRAY);
 		bouttonOk.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		bouttonOk.setBounds(267, 170, 89, 23);
-
-		// on ajoute le boutton a la JFrame
 		frame.getContentPane().add(bouttonOk);
-
-		// fin bouttonOk ***********************************************
-
-		// début label fondSaisieNom qui affiche l'image de fond
 
 		JLabel fondSaisieNom = new JLabel("");
 		fondSaisieNom
 				.setIcon(new ImageIcon(InterfaceSaisieNom.class.getResource("/cda/poo/images/fondMenuDemarrer.jpg")));
 		fondSaisieNom.setHorizontalAlignment(SwingConstants.CENTER);
 		fondSaisieNom.setBounds(0, 0, 634, 261);
-
-		// on ajoute le label a la JFrame
 		frame.getContentPane().add(fondSaisieNom);
-
-		// fin lbel fondSaisieNom ***********************************************
-
-		// début label saisieNom ***********************************************
 
 		JLabel saisieNom = new JLabel("Veuillez saisir un nom entre 3 et 6 lettre");
 		saisieNom.setForeground(new Color(255, 255, 240));
 		saisieNom.setFont(new Font("Times New Roman", Font.BOLD, 29));
 		saisieNom.setBounds(70, 11, 496, 71);
 		fondSaisieNom.add(saisieNom);
-
-		// fin label saisieNom ***********************************************
 	}
 
-	// méthode qui retourne en string le nom du joueur
-	// ***********************************************
 	public static String getJoueurActuel() {
 		return joueurActuel.getNom();
 	}
--- cdaProjetAvion/src/cda/poo/json/JsonTools.java
+++ cdaProjetAvion/src/cda/poo/json/JsonTools.java
@@ -2,33 +2,36 @@ package cda.poo.json;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import cda.interfaceGraphique.InterfaceJeu;
+
+import cda.interfaceGraphique.InterfaceSaisieNom;
 import cda.json.JSONException;
 import cda.json.JSONObject;
+import cda.poo.meteor.Score;
 import cda.poo.objects.Joueur;
+
 public class JsonTools {
 
-	private static Joueur j;
-	public static void main(String[] args) {
-	}
+	public static Joueur j;
 
-//	méthode pour créer ou éditer un fichier .json
+	public static void main(String[] args) {
+
+	}
 
 	public static void jsonWrite() {
 
 		String nomFichier = "scores.json";
 		JSONObject jsonObject = new JSONObject();
-		
+
 		try {
 			jsonObject.put("date", DateTools.getDate());
-			jsonObject.put("score", InterfaceJeu.getScore());
-			jsonObject.put("name", InterfaceJeu.getNom());
-			
+			jsonObject.put("score", Score.getScoreMeteor());
+			jsonObject.put("name", InterfaceSaisieNom.getJoueurActuel());
+
 			FileWriter file = new FileWriter(nomFichier, true);
-			
+
 			file.write(jsonObject.toString());
 			file.close();
-			
+
 		} catch (JSONException e1) {
 			e1.printStackTrace();
 		} catch (IOException e) {
